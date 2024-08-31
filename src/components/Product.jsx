8a3d8221e6907b0efb925c853d3f3cebe1bbccf0
Rajagopal37 <trajagopal37@gmail.com>
2024-08-31 17:36:15 +0530

    added button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,36 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../Features/ContextProvider";
 
 const Product = ({ product }) => {
-  const { dispatch } = useContext(CartContext);
+  const { cart, dispatch } = useContext(CartContext);
+  const [isAdded, setIsAdded] = useState(false);
+
+  useEffect(() => {
+    // Check if the product is in the cart
+    const productInCart = cart.find((item) => item.id === product.id);
+    setIsAdded(!!productInCart); // Set isAdded to true if the product is in the cart, false otherwise
+  }, [cart, product.id]);
+
+  const addToCart = () => {
+    dispatch({ type: "Add", product: product });
+    setIsAdded(true);
+  };
 
   return (
-    <div className="conitainer">
+    <div className="container">
       <div className="row my-3">
         <div className="col">
           <div className="card bg-light">
             <div className="image text-center">
               <img
                 src={product.image}
-                className="card-img-top mt-2 "
+                className="card-img-top mt-2"
                 style={{ width: "200px", height: "200px" }}
+                alt={product.title}
               />
             </div>
 
-            <div className="card-body bg-light gray">
+            <div className="card-body bg-light">
               <h5 className="card-title">{product.title}</h5>
               <p className="card-text">{product.description}</p>
               <li className="list-group-item">
-                Price :<span className="fw-bolder"> ₹ {product.price} </span>
+                Price: <span className="fw-bolder">₹ {product.price}</span>
               </li>
             </div>
 
             <div className="card-body d-flex">
               <button
                 className="btn btn-primary mx-2"
-                onClick={() => dispatch({ type: "Add", product: product })}
+                onClick={addToCart}
+                disabled={isAdded}
               >
-                Add to cart
+                {isAdded ? "Added" : "Add to Cart"}
               </button>
               <button className="btn btn-success mx-2">Buy Now</button>
             </div>
