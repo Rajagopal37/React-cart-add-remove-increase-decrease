b03ff492848c8911f6729c6ffe0104ef35cd8476
Rajagopal37 <trajagopal37@gmail.com>
2024-08-31 16:31:14 +0530

    dep

--- src/Features/ContextProvider.jsx
+++ src/Features/ContextProvider.jsx
@@ -1,16 +1,16 @@
-// import React, { createContext, useReducer } from "react";
-// import CartReducer from "./CartReducer";
+import React, { createContext, useReducer } from "react";
+import CartReducer from "./CartReducer";
 
-// export const CartContext = createContext();
+export const CartContext = createContext();
 
-// const ContextProvider = ({ children }) => {
-//   const [cart, dispatch] = useReducer(CartReducer, []);
+const ContextProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(CartReducer, []);
 
-//   return (
-//     <CartContext.Provider value={{ cart, dispatch }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 
-// export default ContextProvider;
+export default ContextProvider;
